cacfb6e7ba8d6da33e40f8a0a1b30e9196df09dd
Fosco Marotto <f@gab.com>
2021-02-10 00:41:25 -0500

    Possible doorkeeper fix

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -63,10 +63,10 @@ class Api::BaseController < ApplicationController
   end
 
   def current_resource_owner
-    if doorkeeper_token
-      @current_user ||= Rails.cache.fetch("dk:user:#{doorkeeper_token.resource_owner_id}", expires_in: 25.hours) do
-        ActiveRecord::Base.connected_to(role: :writing) do
-          User.find(doorkeeper_token.resource_owner_id)
+    ActiveRecord::Base.connected_to(role: :writing) do
+      if doorkeeper_token
+        @current_user ||= Rails.cache.fetch("dk:user:#{doorkeeper_token.resource_owner_id}", expires_in: 25.hours) do
+            User.find(doorkeeper_token.resource_owner_id)
         end
       end
     end
