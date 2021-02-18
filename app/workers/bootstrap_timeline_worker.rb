4696307cc96d47d6bd6f196d4a1b298ee95c8e7c
Fosco Marotto <f@gab.com>
2021-02-18 15:02:16 -0500

    Use a writing role for some sidekiq worker reads

--- app/workers/bootstrap_timeline_worker.rb
+++ app/workers/bootstrap_timeline_worker.rb
@@ -4,6 +4,8 @@ class BootstrapTimelineWorker
   include Sidekiq::Worker
 
   def perform(account_id)
-    BootstrapTimelineService.new.call(Account.find(account_id))
+    ActiveRecord::Base.connected_to(role: :writing) do
+      BootstrapTimelineService.new.call(Account.find(account_id))
+    end
   end
 end
--- app/workers/local_notification_worker.rb
+++ app/workers/local_notification_worker.rb
@@ -8,10 +8,12 @@ class LocalNotificationWorker
   def perform(receiver_account_id, activity_id = nil, activity_class_name = nil)
     return true if activity_id.nil? or activity_class_name.nil?
 
-    receiver = Account.find(receiver_account_id)
-    activity = activity_class_name.constantize.find(activity_id)
+    ActiveRecord::Base.connected_to(role: :writing) do
+      receiver = Account.find(receiver_account_id)
+      activity = activity_class_name.constantize.find(activity_id)
 
-    NotifyService.new.call(receiver, activity)
+      NotifyService.new.call(receiver, activity)
+    end
   rescue ActiveRecord::RecordNotFound
     true
   end
